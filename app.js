90cb308ca3288d9e42c993308a073b6f9bb41cbf
Miho19 <miho19@hotmail.co.nz>
2024-04-18 21:39:33 +1200

    Adding in index route, cookieparser

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const indexRouter = require("./routes/index");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(indexRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`);
 });
+
+module.exports = app;
