b60b524c8f9a6182db0f8234b8a8e50604598ef9
Miho19 <miho19@hotmail.co.nz>
2024-04-19 14:56:50 +1200

    Added a generic test to confirm we have no index route

---
+++ tests/playlist.test.js
@@ -0,0 +1,52 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("Playlist API", () => {
+  it("GET /playlist, returns playlist", () => {
+    return request(app)
+      .get("/playlist")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({ name: expect.any(String) }),
+          ])
+        );
+      });
+  });
+
+  it("GET /playlist/:index, returns a specific song from that playlist", () => {
+    return request(app)
+      .get("/playlist/1")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual(
+          expect.objectContaining({ name: expect.any(String) })
+        );
+      });
+  });
+
+  it("GET /playlist/:index, 404 if not found", () => {
+    return request(app)
+      .get("/playlist/9999999")
+      .expect("Content-Type", /json/)
+      .expect(404);
+  });
+
+  it("PATCH /playlist, adding a song to the playlist", () => {
+    return request(app)
+      .patch("/playlist")
+      .send({ name: "new song" })
+      .expect("Content-Type", /json/)
+      .expect(201)
+      .then((res) => {
+        expect(res.body).toEqual(expect.objectContaining({ name: "new song" }));
+      });
+  });
+
+  it("GET /playlist", () => {
+    return request(app).patch("/playlist").send({ name: 123 }).expect(422);
+  });
+});
